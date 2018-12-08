157eebebfd346cc5c790a1e8ff0a610b6cc69213
AndersonMarquess <andersongabriel96.ag@gmail.com>
2018-12-08 14:55:26 -0200

    Criando primeiro controle de animação

--- Jogo_de_zumbi/Assets/JogadorController.cs
+++ Jogo_de_zumbi/Assets/JogadorController.cs
@@ -20,5 +20,13 @@ public class JogadorController : MonoBehaviour {
 
         Vector3 direcao = new Vector3(horizontal, 0, vertical);
         transform.Translate(direcao * velocidade * Time.deltaTime);
+
+        //Verifica se é o personagem está parado na posição zero com o vector 3
+        var andando = direcao != Vector3.zero;
+        tocarAnimacaoAndar(andando);
+    }
+
+    private void tocarAnimacaoAndar(bool resultado) {
+        transform.GetComponent<Animator>().SetBool("Andando", resultado);
     }
 }
