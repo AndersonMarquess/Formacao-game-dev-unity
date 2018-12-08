93e0abbb9418522ff6b13dc50c3876c151a2416c
AndersonMarquess <andersongabriel96.ag@gmail.com>
2018-12-08 15:35:17 -0200

    Criado Controller para câmera e pasta de scripts

---
+++ Jogo_de_zumbi/Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private GameObject jogador;
+    public Vector3 compensarDistancia;
+
+    /// <summary>
+    /// Ao iniciar procura o gameobject com nome jogador para ajustar a posição da câmera,
+    /// com base no valor da variável compensarDistancia.
+    /// </summary>
+    void Start() {
+        jogador = GameObject.FindGameObjectWithTag("Jogador");
+        compensarDistancia = new Vector3(0, 15, -10);
+    }
+
+    void Update() {
+        atualizarPosicao();
+        olharPlayer();
+    }
+
+    /// <summary>
+    /// Coloca a câmera na posição do jogador mais a distância de compensação, para que a câmera não entre dentro do jogador.
+    /// </summary>
+    private void atualizarPosicao() {
+        transform.position = jogador.transform.position + compensarDistancia;
+    }
+
+    /// <summary>
+    /// Mantem a câmera olhando fixamente para o jogador.
+    /// </summary>
+    private void olharPlayer() {
+        transform.LookAt(jogador.transform);
+    }
+}
--- Jogo_de_zumbi/Assets/Scripts/JogadorController.cs
+++ Jogo_de_zumbi/Assets/Scripts/JogadorController.cs
@@ -1,15 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class JogadorController : MonoBehaviour {
 
     public int velocidade = 10;
 
+    /// <summary>
+    /// Executa quando o script está sendo carregado e atribui a tag Jogador ao gameobject
+    /// </summary>
+    private void Awake() {
+        transform.tag = "Jogador";
+    }
+
     void Update() {
         movimentacao();
     }
-
 
     /// <summary>
     /// Movimenta o personagem com base no input recebido
@@ -21,7 +25,7 @@ public class JogadorController : MonoBehaviour {
         Vector3 direcao = new Vector3(horizontal, 0, vertical);
         transform.Translate(direcao * velocidade * Time.deltaTime);
 
-        //Verifica se é o personagem está parado na posição zero com o vector 3
+        //Verifica se o personagem está parado na posição zero com o vector 3
         var andando = direcao != Vector3.zero;
         tocarAnimacaoAndar(andando);
     }
