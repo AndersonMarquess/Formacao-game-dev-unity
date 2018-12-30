8080ef1d4d17a09a114dec38019a37df83a4b0bd
AndersonMarquess <andersongabriel96.ag@gmail.com>
2018-12-30 19:34:31 -0200

    Criado lógica para animação de ataque dos inimigos

--- Jogo_de_zumbi/Assets/Scripts/InimigoController.cs
+++ Jogo_de_zumbi/Assets/Scripts/InimigoController.cs
@@ -3,15 +3,22 @@
 public class InimigoController : MonoBehaviour {
 
     private GameObject alvo;
+    private Rigidbody rb;
     public float velocidadeMovimentacao = 3;
     public float distanciaParada = 2.3f;
 
     void Start() {
+        rb = rb = GetComponent<Rigidbody>();
         alvo = GameObject.FindGameObjectWithTag("Jogador");
     }
 
     void FixedUpdate() {
-        if(distanciaDoAlvo() > distanciaParada) {
+        rotacionarInimigo(direcaoAteOAlvo());
+
+        var isAlvoLonge = distanciaDoAlvo() > distanciaParada;
+        GetComponent<Animator>().SetBool("Atacando", !isAlvoLonge);
+
+        if(isAlvoLonge) {
             seguirAlvo();
         }
     }
@@ -20,22 +27,32 @@ public class InimigoController : MonoBehaviour {
     /// Adiciona força ao rigidbody para mover-se até o alvo.
     /// </summary>
     private void seguirAlvo() {
-        var direcao = alvo.transform.position - transform.position;
-        var rb = GetComponent<Rigidbody>();
-        rb.MovePosition(rb.position + direcao.normalized * velocidadeMovimentacao * Time.deltaTime);
+        rb.MovePosition(rb.position + direcaoAteOAlvo().normalized * velocidadeMovimentacao * Time.deltaTime);
 
-        rotacionarInimigo(rb, direcao);
+        rotacionarInimigo(direcaoAteOAlvo());
+    }
+
+    /// <summary>
+    /// Com base na posição do alvo é retornado o valor em Vector3 até sua posição.
+    /// </summary>
+    /// <returns>distância até o alvo em Vector3</returns>
+    private Vector3 direcaoAteOAlvo() {
+        return alvo.transform.position - transform.position;
     }
 
     /// <summary>
     /// Verifica a distância entre o inimigo e o alvo, retorna o valor da distância.
     /// </summary>
-    /// <returns></returns>
+    /// <returns>float com a distância até o alvo</returns>
     private float distanciaDoAlvo() {
         return Vector3.Distance(transform.position, alvo.transform.position);
     }
 
-    private void rotacionarInimigo(Rigidbody rb, Vector3 direcao) {
+    /// <summary>
+    /// Rotaciona o inimigo na direção do player
+    /// </summary>
+    /// <param name="direcao"></param>
+    private void rotacionarInimigo(Vector3 direcao) {
         Quaternion rotacao = Quaternion.LookRotation(direcao);
         rb.MoveRotation(rotacao);
     }
