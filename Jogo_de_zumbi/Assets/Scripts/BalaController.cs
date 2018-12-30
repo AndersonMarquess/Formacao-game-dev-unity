b1b4d8309fa7595429240841ab535fa090746197
AndersonMarquess <andersongabriel96.ag@gmail.com>
2018-12-30 19:08:28 -0200

    Usando colisao com trigger para destruir a bala e o inimigo

--- Jogo_de_zumbi/Assets/Scripts/BalaController.cs
+++ Jogo_de_zumbi/Assets/Scripts/BalaController.cs
@@ -10,4 +10,15 @@ public class BalaController : MonoBehaviour {
         //transform.forward é referente ao eixo Z
         rb.MovePosition(rb.position + transform.forward * velocidadeDisparo * Time.deltaTime);
     }
+
+    /// <summary>
+    /// Ao colidir com outro objeto a bala é destruída, se o outro objeto for um inimigo ele também é destruído.
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Inimigo")
+            Destroy(other.gameObject);
+
+        Destroy(gameObject);
+    }
 }
