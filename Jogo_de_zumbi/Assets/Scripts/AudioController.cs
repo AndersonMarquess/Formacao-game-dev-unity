82d33d894913ae38df70d50b10dfa1c0f5538dfb
AndersonMarquess <andersongabriel96.ag@gmail.com>
2019-01-13 13:04:22 -0200

    Criado sistema de audio para cena inicial

---
+++ Jogo_de_zumbi/Assets/Scripts/AudioController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public static AudioSource audioSourceGeral;
+
+    void Awake() {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSourceGeral = audioSource;
+    }
+}
