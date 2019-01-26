75634358926bac325f14d6177477e365831f4a4d
AndersonMarquess <andersongabriel96.ag@gmail.com>
2019-01-26 13:37:24 -0200

    Adicionado NavMeshAgent para o chefe do primeiro nível

---
+++ Jogo_de_zumbi/Assets/Scripts/ChefeController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class ChefeController : MonoBehaviour {
+
+    private Transform _jogador;
+    private NavMeshAgent _agent;
+
+    private void Start() {
+        _jogador = GameObject.FindWithTag(Tags.Jogador).transform;
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update() {
+        definirDestino();
+    }
+
+    /// <summary>
+    /// Faz o chefão seguir a posição do jogador.
+    /// </summary>
+    private void definirDestino() {
+        _agent.SetDestination(_jogador.position);
+    }
+}
